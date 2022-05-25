c7ac51357f49886e69c67e66bd2c5014a8e0babf
Florczak David <florczakdavid@gmail.com>
2022-05-25 14:01:55 +0200

    Initial Commit

---
+++ testMarmalade/testMarmaladeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMarmaladeApp.swift
+//  testMarmalade
+//
+//  Created by david florczak on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct testMarmaladeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
