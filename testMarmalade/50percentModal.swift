f55a4aac90e81b353e3ad3ff69f60c4968689afd
Florczak David <florczakdavid@gmail.com>
2022-05-25 15:57:36 +0200

    working logic without api call

---
+++ testMarmalade/50percentModal.swift
@@ -0,0 +1,54 @@
+//
+//  50percentModal.swift
+//  testMarmalade
+//
+//  Created by david florczak on 25/05/2022.
+//
+
+import SwiftUI
+
+struct _0percentModal: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var onDismiss: ((_ model: Bool) -> Void)?
+    @Binding var showEnd: Bool
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.white)
+            VStack {
+                Text("Tu es à 50% des citations")
+                    .font(.headline)
+                    .foregroundColor(Color.orange)
+                Text("Tu a bientôt finis!")
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+                HStack {
+                    Button {
+                        showEnd = true
+                        onDismiss?(showEnd)
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Arrêter")
+                    }.buttonStyle(.bordered)
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Continuer")
+                    }.buttonStyle(.bordered)
+                }
+            }
+        }
+        .scaledToFit()
+        .padding()
+        .background(Color.gray)
+    }
+}
+
+struct _0percentModal_Previews: PreviewProvider {
+    static var previews: some View {
+        _0percentModal(showEnd: .constant(false))
+    }
+}
--- testMarmalade/ContentView.swift
+++ testMarmalade/ContentView.swift
@@ -8,9 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var author = "{auteur}"
+    @State var citation = "{citation}"
+    @State var smiley = ["🤧","😕","🤩"]
+    @State var progression: Int = 0
+    @State private var showingModal = false
+    
+    @State var showEnd = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        if showEnd {
+            VStack {
+                Text("finis!")
+                Button("Recommencer") {
+                    progression = 0
+                    showEnd = false
+                }
+            }
+            
+        }
+        else {
+            VStack {
+                Spacer()
+                Text(author)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                Text(citation)
+                    .foregroundColor(.white)
+                    .font(.headline)
+                Text(smiley[progression/40]) // passe de 0 à 1 puis 2 quand la progression atteind 40 et 80
+                    .font(.system(size: 120))
+                Spacer()
+                ZStack {
+                    Rectangle()
+                        .fill(Color.white)
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .padding(.bottom, -20)
+                    VStack {
+                        Text("\(progression) % des citations")
+                            .font(.largeTitle)
+                        HStack {
+                            ProgressView(value: Float(progression)/100)
+                                .progressViewStyle(.linear)
+                                .tint(.orange)
+                            
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.blue)
+                        }
+                        Text("10 citations")
+                        Button {
+                            progression += 10
+                            if progression == 50 {
+                                showingModal = true
+                            }
+                            if progression == 110 {
+                                showEnd = true
+                            }
+                            fetchCitations()
+                        } label: {
+                            Text("Citation suivante")
+                        }.buttonStyle(.borderedProminent)
+                            .tint(Color("buttonColor"))
+                    }
+                }
+            }.background(Color.orange)
+                .sheet(isPresented: $showingModal) {
+                    _0percentModal(showEnd: $showEnd)
+            }
+        }
+    }
+    
+    func fetchCitations() {
+        
     }
 }
 
